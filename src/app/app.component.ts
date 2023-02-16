6efda5a08cb8ff5247897e6436bcd0b0d070e35b
cbarriuso <carlosbarriuso@yahoo.es>
2023-02-16 17:52:13 +0100

    Modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root', // nombre del componente principal
-  templateUrl: 'app.component.html',  // archivo html para este componente
+  templateUrl: './app.component.html',  // archivo html para este componente
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
