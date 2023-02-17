f4ab18a91e7d65113fe8f9485e97af4fa12fc0c4
cbarriuso <carlosbarriuso@yahoo.es>
2023-02-17 10:32:07 +0100

    listado personajes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+   
     
   ],
   imports: [
     BrowserModule,
     HeroesModule, 
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
